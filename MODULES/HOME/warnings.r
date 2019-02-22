44634d901d79cc1dc7b83401aa64c1002a17b961
Veen <d.veen@uu.nl>
2019-02-22 16:35:18 -0500

    Update warnings.r
    now with html output so that opacity can be used

--- MODULES/HOME/warnings.r
+++ MODULES/HOME/warnings.r
@@ -11,7 +11,8 @@ warningsUI <- function (id) {
 
 warnings <- function (input, output, session) {
   
-  output$warningMessageDivergence <- renderText({
+  output$divergence <- renderUI({
+    ns <- session$ns
     
     checkDivergences <- lapply(sso@sampler_params, "[", , "divergent__") %>%
       lapply(., as.data.frame) %>%
@@ -23,39 +24,25 @@ warnings <- function (input, output, session) {
              round((. / ((sso@n_iter-sso@n_warmup) * sso@n_chain)) * 100, 1),
              "%).")
     
-    checkDivergences
-    
-  })
-  
-  output$divergence <- renderUI({
-    ns <- session$ns
-    
-    # linkDiagnoseDivergent <- a("Find out more.", 
-    # href = 'diagnoseHomepage-divergentTransitions')
-    
     if(lapply(sso@sampler_params, "[", , "divergent__") %>%
        lapply(., as.data.frame) %>%
        lapply(., filter, row_number() == (1 + sso@n_warmup) : sso@n_iter) %>%
        lapply(., function (x) x > 0 ) %>% lapply(., sum) %>% 
        unlist(.) %>% sum(.) > 0) {
-      div(
-        style = "background-color:red",
-        align = "center",
-        textOutput(ns("warningMessageDivergence"))
-        # tagList(linkDiagnoseDivergent),
-        # actionLink("diagnoseHomepage-divergentTransitions", "test")
-      ) 
+      HTML(paste0("<div style='background-color:red; color:white; 
+                    padding:5px; opacity:.3'>",
+                  checkDivergences, "</div>"))
     } else {
-      div(
-        style = "background-color:lightblue",
-        align = "center",
-        textOutput(ns("warningMessageDivergence"))
-      )
+      HTML(paste0("<div style='background-color:lightblue; color:black; 
+                  padding:5px; opacity:.3'>",
+                  checkDivergences, "</div>"))
     }
+    
   })
   
   
-  output$warningMessageTreedepth <- renderText({
+  output$treedepth <- renderUI({
+    ns <- session$ns
     
     check_treedepth <- lapply(sso@sampler_params, "[", , "treedepth__") %>%
       lapply(., as.data.frame) %>%
@@ -68,98 +55,78 @@ warnings <- function (input, output, session) {
              round((. / ((sso@n_iter-sso@n_warmup) * sso@n_chain)) * 100, 1),
              "%).")
     
-      check_treedepth
-  })
-  
-  output$treedepth <- renderUI({
-    ns <- session$ns
-    
     if(lapply(sso@sampler_params, "[", , "treedepth__") %>%
        lapply(., as.data.frame) %>%
        lapply(., filter, row_number() == (1 + sso@n_warmup) : sso@n_iter) %>%
        lapply(., function (x) x == sso@misc$max_td ) %>% 
        lapply(., sum) %>% unlist(.) %>% sum(.) > 0) {
-      div(
-        style = "background-color:red",
-        align = "center",
-        textOutput(ns("warningMessageTreedepth"))
-      ) 
+        HTML(paste0("<div style='background-color:red; color:white; 
+                    padding:5px; opacity:.3'>",
+                    check_treedepth, "</div>"))
     } else {
-      div(
-        style = "background-color:lightblue",
-        align = "center",
-        textOutput(ns("warningMessageTreedepth"))
-      )
+      HTML(paste0("<div style='background-color:lightblue; color:black; 
+                  padding:5px; opacity:.3'>",
+                  check_treedepth, "</div>"))
     }
   })
   
-  
-  
-  check_energy_sso <- function(shinystan.object){
-    
-    energy <- lapply(sso@sampler_params, "[", , "energy__") %>%
-      lapply(., as.data.frame) %>% 
-      lapply(., filter, row_number() == (1 + sso@n_warmup):sso@n_iter) 
-    
-    EBFMIs <- c()
-    
-    for(chain in 1:sso@n_chain) {
-      EBFMIs[chain] <- apply(energy[[chain]], 2, function(x){
-        numer <- sum(diff(x)^2)/length(x)
-        denom <- var(x)
-        return(numer/denom)
-      })
-    }
-    
-    bad_chains <- which(EBFMIs < 0.2)
-    if (!length(bad_chains)) {
-      paste0("E-BFMI indicated no pathological behavior.")
-    }
-    else {
-      EBFMIsWarnings <- NULL
-      for(bad in bad_chains){
-        EBFMIsWarnings <- c(EBFMIsWarnings, 
-                            paste0("Chain ", bad, ": E-BFMI = ", EBFMIs[bad], ".\n"))
-      }
-      cat(paste0("E-BFMI indicated possible pathological behavior:\n", 
-                 paste(EBFMIsWarnings, collapse = "") ,
-                 "E-BFMI below 0.2 indicates you may need to reparameterize your model."), 
-          sep = "\n")
-    }
-    
-  }
-  
-  
-  output$warningMessageEnergy <- renderText({
-    
-    check_energy_sso(sso)
-    
-  })
-
-  
+ 
   output$energy <- renderUI({
     ns <- session$ns
     
+    check_energy_sso <- function(shinystan.object){
+      
+      energy <- lapply(sso@sampler_params, "[", , "energy__") %>%
+        lapply(., as.data.frame) %>% 
+        lapply(., filter, row_number() == (1 + sso@n_warmup):sso@n_iter) 
+      
+      EBFMIs <- c()
+      
+      for(chain in 1:sso@n_chain) {
+        EBFMIs[chain] <- apply(energy[[chain]], 2, function(x){
+          numer <- sum(diff(x)^2)/length(x)
+          denom <- var(x)
+          return(numer/denom)
+        })
+      }
+      
+      bad_chains <- which(EBFMIs < 0.2)
+      if (!length(bad_chains)) {
+        paste0("E-BFMI indicated no pathological behavior.")
+      }
+      else {
+        EBFMIsWarnings <- NULL
+        for(bad in bad_chains){
+          EBFMIsWarnings <- c(EBFMIsWarnings, 
+                              paste0("Chain ", bad, ": E-BFMI = ", EBFMIs[bad], ".\n"))
+        }
+        cat(paste0("E-BFMI indicated possible pathological behavior:\n", 
+                   paste(EBFMIsWarnings, collapse = "") ,
+                   "E-BFMI below 0.2 indicates you may need to reparameterize your model."), 
+            sep = "\n")
+      }
+      
+    }
+    
+    energyWarning <- check_energy_sso(sso)
+    
+    
     if(check_energy_sso(sso) != "E-BFMI indicated no pathological behavior.") {
-      div(
-        style = "background-color:red",
-        align = "center",
-        textOutput(ns("warningMessageEnergy"))
-      ) 
+      HTML(paste0("<div style='background-color:red; color:white; 
+                padding:5px; opacity:.3'>",
+                  energyWarning, "</div>"))
     } else {
-      div(
-        style = "background-color:lightblue",
-        align = "center",
-        textOutput(ns("warningMessageEnergy"))
-      )
+      HTML(paste0("<div style='background-color:lightblue; color:black; 
+                padding:5px; opacity:.3'>",
+                  energyWarning, "</div>"))
     }
   })
-  
   
   
   # sso@summary[, "n_eff"] / ((sso@n_iter- sso@n_warmup) * sso@n_chain) # should be over .1
   # sso@summary[, "se_mean"] / sso@summary[, "sd"] # should be under .1
   # sso@summary[, "Rhat"] # should be under 1.1
+  
   
   
 }
