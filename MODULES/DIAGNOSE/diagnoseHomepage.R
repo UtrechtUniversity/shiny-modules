3fdd563e57787df73578d1c6f36256b70e649330
Veen <d.veen@uu.nl>
2019-02-26 09:43:50 -0500

    Update diagnoseHomepage.R

--- MODULES/DIAGNOSE/diagnoseHomepage.R
+++ MODULES/DIAGNOSE/diagnoseHomepage.R
@@ -3,19 +3,44 @@ diagnoseUI <- function(id){
   ns <- NS(id)
   
   # encapsulate everything in taglist, see https://shiny.rstudio.com/articles/modules.html
-  tagList(
-    tabsetPanel(
-      id = ns("diagnose_tabset"),
-      nonHMCdiagnosticsUI(ns("non-HMC")),
-      if(sso@misc$stan_method == "sampling") HMCdiagnosticsUI(ns("HMC"))
-    )
-  )
- 
+  # tagList(
+  #   tabsetPanel(
+  #     id = ns("diagnose_tabset"),
+  #     nonHMCdiagnosticsUI(ns("non-HMC")),
+  #     if(sso@misc$stan_method == "sampling") HMCdiagnosticsUI(ns("HMC"))
+  #   )
+  # )
+  uiOutput(ns("diagnoseHomepage"))
+  
 }
 
 diagnose <- function(input, output, session){
   
   callModule(nonHMCdiagnostics, "non-HMC")
-  if(sso@misc$stan_method == "sampling") callModule(HMCdiagnostics, "HMC")
+  callModule(HMCdiagnostics, "HMC")
+  
+  
+  if(sso@misc$stan_method == "sampling") {
+    output$diagnoseHomepage <- renderUI({
+      
+      tagList(
+        tabsetPanel(
+          id = session$ns("diagnose_tabset"),
+          HMCdiagnosticsUI(session$ns("HMC")),
+          nonHMCdiagnosticsUI(session$ns("non-HMC"))
+        )
+      )
+    })
+  } else {
+    output$diagnoseHomepage <- renderUI({
+      
+      tagList(
+        tabsetPanel(
+          id = session$ns("diagnose_tabset"),
+          nonHMCdiagnosticsUI(session$ns("non-HMC"))
+        )
+      )
+    })
+  }
   
 }
